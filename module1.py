f369a0de62e3d088e32722756c7ba654f10da58a
opilane <opilane@HP-CZC6297BBB>
2021-12-02 08:49:13 +0200

    a

--- module1.py
+++ module1.py
@@ -1,76 +1,76 @@
 ﻿def lists()->list:
-"""tegi listid failist
-"""
-palgad=[]
-with open("zarplata.txt", "r") as f1: #avame fail
-for s in f1:
-palgad.append(s.strip()) # tegi listid
-inimesed=[]
-with open ("ludi.txt", "r") as inimene:
-for q in inimene:
-inimesed.append(q.strip())
-return palgad,inimesed
+    """ tegi listid failist
+    """
+    palgad=[]
+    with open("zarplata.txt", "r") as f1: #avame fail
+        for s in f1:
+            palgad.append(s.strip()) # tegi listid
+            inimesed=[]
+            with open ("ludi.txt", "r") as inimene:
+                for q in inimene:
+                    inimesed.append(q.strip())
+                    return palgad,inimesed
 
-def add_person ():
-""" lisamine inimese ja palk
-"""
-nimi=input("Siseta nimi: ")
-palga=input("Siseta palgad: ")
-with open("ludi.txt", "a") as inimesed: #lisame nimi failisse
-inimesed.write(nimi+"\n")
-with open("zarplata.txt", "a") as palgad: #lisame palk
-palgad.write(palga+"\n")
+def add_inimene ():
+    """ lisamine inimese ja palk
+    """
+    nimi=input("Siseta nimi: ")
+    palga=input("Siseta palgad: ")
+    with open("ludi.txt", "a") as inimesed: #lisame nimi failisse
+        inimesed.write(nimi+"\n")
+        with open("zarplata.txt", "a") as palgad: #lisame palk
+            palgad.write(palga+"\n")
 
-def smallest_salary():
-"""arvutus on väikseim palk
-"""
-palgad,inimesed=lists()
-palgadS=palgad.copy()
-palgadS.sort()
-a=palgadS[0]
-b=palgad.index(a)
-print("kõike väiksem palga on "+inimesed[b]+" palga ja see on "+ palgadS[0]+" euro")
+def smal_palk():
+    """arvutus on väikseim palk
+    """
+    palgad,inimesed=lists()
+    palgadS=palgad.copy()
+    palgadS.sort()
+    a=palgadS[0]
+    b=palgad.index(a)
+    print("kõike väiksem palga on "+inimesed[b]+" palga ja see on "+ palgadS[0]+" euro")
 
-def delete_person ():
-"""Eemaldamine mees ja palk
-"""
-palgad,inimesed=lists() #failid ja kasutamine lsitid
-nimi=input("Siseta nimi: ")
-if nimi not in inimesed: #vaatame, kas on olemas selline inimene
-print("Kas sa tahad lisada nimi ja palgad?") #
-c=input("Y = jah, N = ei")
-if c.upper=="Y":
-add_person()
-else:
-pass
-else:
-a=inimesed.index(nimi) #  /otsing  indeks
-inimesed.pop(a) # delet nimi
-palgad.pop(a) # delet palk
-f=open("ludi.txt", "w")
-for g in inimesed:
-f.write(g+"\n")
-f.close()
-d=open("zarplata.txt", "w")
-for i in palgad:
-d.write(i+"\n")
-d.close()
+def delete_inimene():
+    """Eemaldamine mees ja palk
+    """
+    palgad,inimesed=lists() #failid ja kasutamine lsitid
+    nimi=input("Siseta nimi: ")
+    if nimi not in inimesed: #vaatame, kas on olemas selline inimene
+        print("Kas sa tahad lisada nimi ja palgad?") #
+        c=input("Y = jah, N = ei")
+        if c.upper=="Y":
+            add_person()
+            else:
+                pass
+            else:
+                a=inimesed.index(nimi) #  /otsing  indeks
+                inimesed.pop(a) # delet nimi
+                palgad.pop(a) # delet palk
+                f=open("ludi.txt", "w")
+                for g in inimesed:
+                    f.write(g+"\n")
+                    f.close()
+                    d=open("zarplata.txt", "w")
+                    for i in palgad:
+                        d.write(i+"\n")
+                        d.close()
 
-def biggest_salary():
-"""Arvutamine suurim palk
-"""
-palgad,inimesed=lists()
-suurim=max(palgad)
-b=palgad.index(suurim) # indeks muutuja edasiseks kasutamiseks
-print("kõike suured palga on "+inimesed[b]+" palga")
+def big_palk():
+    """Arvutamine suurim palk
+    """
+    palgad,inimesed=lists()
+    suurim=max(palgad)
+    b=palgad.index(suurim) # indeks muutuja edasiseks kasutamiseks
+    print("kõike suured palga on "+inimesed[b]+" palga")
 
-def sorting():
-"""sordi palk nimedega kasvavas järjekorras
-"""
-palgadS=[]
-palgad,inimesed=lists()
-palgadS=palgad.copy()
-palgadS.sort()
-for palk in palgadS:
-a=palgad.index(palk) #  otsime indeks et leida nimi ja palk sorteerimata loendites
-print(palgad[a]+" "+inimesed[a])
+def sortirovka():
+    """sordi palk nimedega kasvavas järjekorras
+    """
+    palgadS=[]
+    palgad,inimesed=lists()
+    palgadS=palgad.copy()
+    palgadS.sort()
+    for palk in palgadS:
+        a=palgad.index(palk) #  otsime indeks et leida nimi ja palk sorteerimata loendites
+        print(palgad[a]+" "+inimesed[a])
--- palgad.py
+++ palgad.py
@@ -2,16 +2,15 @@ from module1 import *
 while True:
 a=input("Funktsioonid: \n delet-1\n Add-2\n sortirovka-3\n min-4\n biggest-5\n")
 if a=="1":
-delete_person()
+delete_inimene()
 elif a=="2":
-add_person()
+add_inimene()
 elif a=="3":
-sorting()
+sortirovka()
 elif a=="4":
-smallest_salary()
+smal_palk()
 elif a=="5":
-biggest_salary()
-elif a=="7":
+big_palk()
 break
 else:
 ("eta funktsia ne robit")
